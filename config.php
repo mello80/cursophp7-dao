b70a9c3eef5d19d5ce42848c58e789a6d3aff577
Luiz <mello80@gmail.com>
2018-07-27 13:12:42 -0300

    Iniciamos o projeto

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+spl_autoload_register(function($classname){
+
+	$filename = "class" . DIRECTORY_SEPARATOR .  $classname . ".php";
+
+	if (file_exists(($filename))) {
+		
+		require_once($filename);
+	}
+
+});
+
+
+ ?>
